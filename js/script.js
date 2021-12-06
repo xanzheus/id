43878a195567c863078697cd81fc2d4346ded564
afifrohul <afifmemyself22@gmail.com>
2021-12-07 00:39:45 +0700

    melengkapi about, product, feedback, contact, footer, dan menambah assets

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+const dark = document.querySelector(".dark input");
+const tulisanHero = document.querySelector(".hero");
+const tulisanAbout = document.querySelector(".konten");
+dark.addEventListener("click", function () {
+  document.body.classList.toggle("darkMode");
+  tulisanHero.classList.toggle("tulisanDarkMode");
+  tulisanAbout.classList.toggle("tulisanDarkMode");
+});
